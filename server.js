59be2bd54c744cc6ae16f11bde6d60008cc868fa
Candice Humpherys <smrtsmrf@gmail.com>
2016-07-21 17:56:43 -0600

    done with D1 except front end

---
+++ server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var mongojs = require('mongojs');
+var ObjectId = mongojs.ObjectId;
+
+var port = 3000;
+
+var app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+var db = mongojs('ecommerce', ['products'])
+
+app.post('/api/products', function (req, res, next) {
+	db.products.save(req.body, function(error, response){
+		return error ? res.status(500).json(error) : res.json(response);
+	})
+});
+
+app.get('/api/products', function (req, res, next) {
+	var query = req.query;
+	db.products.find(query, function (error, response) {
+		return error ? res.status(500).json(error) : res.json(response);	
+	})
+});
+
+app.get('/api/products/:id', function (req, res, next) {
+	var idObj = {_id: ObjectId(req.params.id)}
+	db.products.findOne(idObj, function (error, response) {
+		return error ? res.status(500).json(error) : res.json(response);	
+	})
+});
+
+app.put('/api/products/:id', function (req, res, next) {
+	if (!req.params.id) {
+		return res.status(400).send('id query needed');
+	};
+
+	var query = {_id: ObjectId(req.params.id)}
+	db.products.update(query, req.body, function (error, response) {
+		return error ? res.status(500).json(error) : res.json(response);	
+	})
+});
+
+app.delete('/api/products/:id', function (req, res, next) {
+	if (!req.params.id) {
+		return res.status(400).send('id query needed');
+	};
+
+	var query = {_id: ObjectId(req.params.id)}
+	db.products.remove(query, function (error, response) {
+		return error ? res.status(500).json(error) : res.json(response);	
+	})
+});
+
+
+
+app.listen(port, function () {
+	console.log('listening on', port);
+})
